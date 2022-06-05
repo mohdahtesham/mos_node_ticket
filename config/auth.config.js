7d8cafdf1b86f9d879e6bb8037efdca3381778ad
Mohammed Ahtesham <m.ahtesham@investobull.com>
2022-06-05 15:47:47 -0400

    first-ui

---
+++ config/auth.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  secret: "mo-secret-key"
+};
---
+++ config/db.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  url: "mongodb+srv://mo_user:mouser@cluster0.ch79u.mongodb.net/meanApp?retryWrites=true&w=majority"
+};
